f264c97b99abb01f058240d3fabce2642a0f64df
Alexis <utp0007580@alumno.utpuebla.edu.mx>
2021-07-30 18:22:30 -0500

    Master, inicio del proyecto, insercion de clientes

---
+++ ClassLogicaNegocios/LogCliente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassAccesoSQL;
+using ClassEntidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassLogicaNegocios
+{
+    public class LogCliente
+    {
+        private ClaseConeccion objacceso =
+           new ClaseConeccion(@"Data Source=DESKTOP-UJ8LE08; Initial Catalog=PedidosCarniceria; Integrated Security = true;");
+
+        public int idd(Cliente nuevo, ref string mens_salida)
+        {
+            SqlParameter[] params1 = new SqlParameter[5];
+            params1[0] = new SqlParameter
+            {
+                ParameterName = "Nombre",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 90,
+                Direction = ParameterDirection.Input,
+                Value = nuevo.Nombre
+            };
+            params1[1] = new SqlParameter
+            {
+                ParameterName = "App",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 90,
+                Direction = ParameterDirection.Input,
+                Value = nuevo.App
+            };
+            params1[2] = new SqlParameter
+            {
+                ParameterName = "ApM",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 90,
+                Direction = ParameterDirection.Input,
+                Value = nuevo.ApM
+            };
+            params1[3] = new SqlParameter
+            {
+                ParameterName = "Celular",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 20,
+                Direction = ParameterDirection.Input,
+                Value = nuevo.Celular
+            };
+            params1[4] = new SqlParameter
+            {
+                ParameterName = "Correo",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 150,
+                Direction = ParameterDirection.Input,
+                Value = nuevo.Correo
+            };
+
+            int idr = 0;
+            string query = @"insert into Cliente values (@Nombre,@App,@ApM,@Celular,@Correo)
+                            SELECT @@IDENTITY AS 'id_Cliente'; ";
+
+            SqlDataReader cont_atrapa = null;
+
+            cont_atrapa = objacceso.ModificaBDunPocoMasSeguraDS(query, objacceso.AbrirConexion(ref mens_salida),
+                ref mens_salida, params1);
+
+            while (cont_atrapa.Read())
+            {
+                idr = Convert.ToInt32(cont_atrapa["id_Cliente"]);
+            }
+
+            return idr;
+        }
+    }
+}
---
+++ ClassLogicaNegocios/LogUbicacion.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassAccesoSQL;
+using ClassEntidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassLogicaNegocios
+{
+    public class LogUbicacion
+    {
+        private ClaseConeccion objacceso =
+           new ClaseConeccion(@"Data Source=DESKTOP-UJ8LE08; Initial Catalog=PedidosCarniceria; Integrated Security = true;");
+
+        private LogCliente LogicaCliente = new LogCliente();
+
+        public Boolean Insert(Ubicacion ub, Cliente nuevo, ref string mens_salida)
+        {
+            int idC = LogicaCliente.idd(nuevo, ref mens_salida);
+
+            SqlParameter[] params1 = new SqlParameter[8];
+            params1[0] = new SqlParameter
+            {
+                ParameterName = "Col",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 120,
+                Direction = ParameterDirection.Input,
+                Value = ub.Colonia
+            };
+            params1[1] = new SqlParameter
+            {
+                ParameterName = "Call",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 120,
+                Direction = ParameterDirection.Input,
+                Value = ub.Calleynumero
+            };
+            params1[2] = new SqlParameter
+            {
+                ParameterName = "Mun",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 90,
+                Direction = ParameterDirection.Input,
+                Value = ub.Municipio
+            };
+            params1[3] = new SqlParameter
+            {
+                ParameterName = "Ciu",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 90,
+                Direction = ParameterDirection.Input,
+                Value = ub.Ciudad
+            };
+            params1[4] = new SqlParameter
+            {
+                ParameterName = "Ref",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 120,
+                Direction = ParameterDirection.Input,
+                Value = ub.Referencia
+            };
+            params1[5] = new SqlParameter
+            {
+                ParameterName = "Carac",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 100,
+                Direction = ParameterDirection.Input,
+                Value = ub.Caracteristica
+            };
+            params1[6] = new SqlParameter
+            {
+                ParameterName = "CP",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 10,
+                Direction = ParameterDirection.Input,
+                Value = ub.CP
+            };
+            params1[7] = new SqlParameter
+            {
+                ParameterName = "F_C",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input,
+                Value = idC
+            };
+
+            string sentencia = "Insert into Ubicacion values (@Col,@Call,@Mun,@Ciu,@Ref,@Carac,@CP,@F_C);";
+
+            Boolean salida = false;
+
+            salida = objacceso.ModificaBDunPocoMasSegura(sentencia, objacceso.AbrirConexion(ref mens_salida), ref mens_salida, params1);
+
+            return salida;
+        }
+
+    }
+}
